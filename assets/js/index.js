687d1af027cf5fee73af6ff318733cc1f9fa3472
jlm0 <jordymcnab@kryptic.io>
2023-02-20 13:21:03 -0600

    Update: Stylings

---
+++ assets/js/index.js
@@ -0,0 +1,9 @@
+const navbar = document.querySelector(".navbar");
+
+window.onscroll = () => {
+  if (window.scrollY > 50) {
+    navbar.classList.add("nav-active");
+  } else {
+    navbar.classList.remove("nav-active");
+  }
+};
